3225f87a365b7331ac90f656cbe5d42adf30e487
Salih Değirmenci <salihdegirmenci99@gmail.com>
2021-06-20 18:58:04 +0300

    Router Support Added

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import "./App.css";
 import MainDashboard from "./layouts/MainDashboard";
 import Navi from "./layouts/Navi";
 import { Container } from "react-bootstrap";
-
+import { Route } from "react-router-dom";
+import FindJobDashboard from "./layouts/FindJobDashboard";
 
 function App() {
   return (
     <div className="App">
       <Navi />
       <Container className="main-container">
-        <MainDashboard  />
+        <Route exact path="/" component={MainDashboard} />
+        <Route exact path="/advertisements" component={FindJobDashboard} />
       </Container>
-      TODO: Buraya Footer Yaz
+      {/* TODO: Buraya Footer Yaz */}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import "react-multi-carousel/lib/styles.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
