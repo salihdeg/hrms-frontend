3634cd5aa69044002e8c60f893dc950c5cb76ef4
Salih Değirmenci <salihdegirmenci99@gmail.com>
2021-06-20 18:58:52 +0300

    Images Added

--- src/layouts/MainCarousels.jsx
+++ src/layouts/MainCarousels.jsx
@@ -1,44 +1,39 @@
 import React from "react";
 import Carousel from "react-bootstrap/Carousel";
+import { Image } from "react-bootstrap";
+import "../styles/MainCarousel.css"
+import Analysis from "../assets/Analysis.svg"
+import Steps from "../assets/Steps.svg"
+import FindWorker from "../assets/FindWorker.svg"
 
 export default function MainCarousels() {
   return (
       <Carousel fade>
         <Carousel.Item>
-          <img
+          {/* <img
             className="d-block w-100"
-            src="https://wallpaperaccess.com/full/2562630.jpg"
+            src={Analysis}
             alt="First slide"
-          />
+          /> */}
+          <Image alt="Analysis Image" style={{width:899, height:570}} src={Analysis} rounded />
           <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            <h3 className="black-text">Cv'lerinizi şirketlere gösterin!</h3>
+            <p className="black-text">Sisteme Cv'lerinizi ekleyerek istediğiniz işe kavuşun!</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="https://wallpaperaccess.com/full/2562630.jpg"
-            alt="Second slide"
-          />
+        <Image alt="Steps Image" style={{width:899, height:570}} src={Steps} rounded />
 
           <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <h3 className="black-text">Kariyer Basamaklarını Hızlıca Tırmanın!</h3>
+            <p className="black-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="https://wallpaperaccess.com/full/2562630.jpg"
-            alt="Third slide"
-          />
-
+        <Image alt="FindWorker Image" style={{width:899, height:570}} src={FindWorker} rounded />
           <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
+            <h3 className="black-text">Şirketinize uygun çalışanı bulun!</h3>
+            <p className="black-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
