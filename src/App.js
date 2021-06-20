74cfbba18cbcbb1f016286d56ab4f1589e236a56
Salih Değirmenci <salihdegirmenci99@gmail.com>
2021-06-20 16:47:19 +0300

    Project Created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import MainDashboard from "./layouts/MainDashboard";
+import Navi from "./layouts/Navi";
+import { Container } from "react-bootstrap";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navi />
+      <Container className="main-container">
+        <MainDashboard  />
+      </Container>
+      TODO: Buraya Footer Yaz
     </div>
   );
 }
