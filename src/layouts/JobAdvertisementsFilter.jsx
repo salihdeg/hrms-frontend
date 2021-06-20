667ed66a1f32729e97b49b48eaa116df0945b3cb
Salih Değirmenci <salihdegirmenci99@gmail.com>
2021-06-20 19:01:40 +0300

    Advertisement Filter Added

---
+++ src/layouts/JobAdvertisementsFilter.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ListGroup from "react-bootstrap/ListGroup";
+
+export default function JobAdvertisementsFilter() {
+  return (
+    <ListGroup>
+      <ListGroup.Item>Cras justo odio</ListGroup.Item>
+      <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+      <ListGroup.Item>Morbi leo risus</ListGroup.Item>
+      <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
+      <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+    </ListGroup>
+  );
+}
