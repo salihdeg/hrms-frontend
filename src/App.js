5da730cacee9076ee6adfb0f2b73ba37fdb8ce9d
Salih Değirmenci <salihdegirmenci99@gmail.com>
2021-06-23 14:08:35 +0300

    New Route Added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navi from "./layouts/Navi";
 import { Container } from "react-bootstrap";
 import { Route } from "react-router-dom";
 import FindJobDashboard from "./layouts/FindJobDashboard";
+import JobAdvertisementDetail from "./pages/JobAdvertisementDetail";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Container className="main-container">
         <Route exact path="/" component={MainDashboard} />
         <Route exact path="/advertisements" component={FindJobDashboard} />
+        <Route path="/advertisements/:id" component={JobAdvertisementDetail} />
       </Container>
       {/* TODO: Buraya Footer Yaz */}
     </div>
