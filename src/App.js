9f747367e424c3fb8fe95876930695f9c88f6100
Salih Değirmenci <salihdegirmenci99@gmail.com>
2021-06-30 17:27:44 +0300

    Toastify and New Route Added

--- src/App.js
+++ src/App.js
@@ -5,13 +5,17 @@ import { Container } from "react-bootstrap";
 import { Route } from "react-router-dom";
 import FindJobDashboard from "./layouts/FindJobDashboard";
 import JobAdvertisementDetail from "./pages/JobAdvertisementDetail";
+import LoginDashboard from "./layouts/LoginDashboard";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
     <div className="App">
+      <ToastContainer position="bottom-right" />
       <Navi />
       <Container className="main-container">
         <Route exact path="/" component={MainDashboard} />
+        <Route exact path="/login" component={LoginDashboard} />
         <Route exact path="/advertisements" component={FindJobDashboard} />
         <Route path="/advertisements/:id" component={JobAdvertisementDetail} />
       </Container>
