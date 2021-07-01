84662a6558b8abc7fa7700e52af364ae184bd640
Salih Değirmenci <salihdegirmenci99@gmail.com>
2021-07-01 14:07:16 +0300

    New Component Preparing

--- src/pages/JobAdvertisementAdd.jsx
+++ src/pages/JobAdvertisementAdd.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 export default function JobAdvertisementAdd() {
-    return (
-        <div>
-            
-        </div>
-    )
+  const jobAdvertisementValues = {};
+
+  return <div></div>;
 }
