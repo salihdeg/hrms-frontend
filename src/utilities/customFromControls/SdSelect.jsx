bf744aaf0301039c5585758506ea66e75b739fba
Salih Değirmenci <salihdegirmenci99@gmail.com>
2021-07-03 22:32:03 +0300

    Custom Select Component Created

---
+++ src/utilities/customFromControls/SdSelect.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Select from "react-select";
+
+export default function SdSelect({ onChange, options, value, className, labelName }) {
+  const defaultValue = (options, value) => {
+    return options ? options.find((option) => option.value === value) : "";
+  };
+
+  return (
+    <div className="mb-2">
+      <label htmlFor={labelName}>{labelName}</label>
+      <div className={className}>
+        <Select
+          value={defaultValue(options, value)}
+          onChange={(value) => onChange(value)}
+          options={options}
+        />
+      </div>
+    </div>
+  );
+}
